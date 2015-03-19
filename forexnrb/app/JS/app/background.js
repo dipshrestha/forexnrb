b43b56337a752746cdc60347977ecf8d35303c19
ssubedi <sharad.manthali@gmail.com>
2015-03-18 21:08:48 -0400

    Added welcome popup after installing app

---
+++ forexnrb/app/JS/app/background.js
@@ -0,0 +1,11 @@
+﻿function createNotification() {
+    var opt = { type: "basic", title: "Foreign Currency Exchange Rate-Nepal Rastra Bank", message: "Thanks for using forexNRB.", iconUrl: "img/icon.png" }
+    chrome.notifications.create("notificationName", opt, function () { });
+
+    //include this line if you want to clear the notification after 5 seconds
+    setTimeout(function () { chrome.notifications.clear("notificationName", function () { }); }, 10000);
+}
+
+// Set up listeners to trigger the first time registration.
+chrome.runtime.onInstalled.addListener(createNotification);
+//chrome.runtime.onStartup.addListener(createNotification);
