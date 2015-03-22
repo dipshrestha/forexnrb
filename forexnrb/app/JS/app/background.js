bdf4bf53443ccd3e5feb12b01867505f0724ac9a
ssubedi <sharad.manthali@gmail.com>
2015-03-21 21:24:08 -0400

    Added google Analytics

--- forexnrb/app/JS/app/background.js
+++ forexnrb/app/JS/app/background.js
@@ -1,4 +1,18 @@
-﻿function createNotification() {
+﻿
+function createNotification() {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-61048918-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function () {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+     })();
+
     var opt = { type: "basic", title: "Foreign Currency Exchange Rate-Nepal Rastra Bank", message: "Thanks for using forexNRB.", iconUrl: "img/icon.png" }
     chrome.notifications.create("notificationName", opt, function () { });
 
